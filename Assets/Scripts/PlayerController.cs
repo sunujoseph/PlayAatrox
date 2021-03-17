4992ccab3b74eeb8fb874400c621a61b637de7bb
sunujoseph <sunupiousjoseph@gmail.com>
2021-03-17 19:56:11 -0400

    First Commit. WOOOO. Basic Movement.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+
+    public LayerMask movementMask;
+
+    Camera cam;
+
+    PlayerMotor motor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                //Debug.Log("We hit " + hit.collider.name + " " + hit.point);
+                motor.MoveToPoint(hit.point);
+            }
+
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                //Debug.Log("We hit " + hit.collider.name + " " + hit.point);
+                // check interactable
+            }
+
+        }
+
+
+    }
+}
